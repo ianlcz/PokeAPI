dcc7f2e05b72c4914418d4063f98bc9c41226194
Yann LE COZ <yann.lecoz@ynov.com>
2021-02-14 16:11:51 +0100

    Left-panel gets MongoDB data

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -2,26 +2,66 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const numeral = require("numeral");
 
 // Importation du modèle Pokemon
 const Pokemon = require("../models/pokemon.model");
+
+// load a locale
+numeral.register("locale", "fr", {
+  delimiters: {
+    thousands: " ",
+    decimal: ",",
+  },
+  abbreviations: {
+    thousand: "k",
+    million: "m",
+    billion: "b",
+    trillion: "t",
+  },
+  ordinal: (number) => (number === 1 ? "er" : "ème"),
+  currency: {
+    symbol: "€",
+  },
+});
+
+// switch between locales
+numeral.locale("fr");
 
 /*
   Get routes
 */
 router.get("/", async (req, res) => {
-  res.render("pokemon/index", {
-    title: "Pokémons",
-    baseUrl: req.baseUrl,
-    pokemons: await Pokemon.find().sort({ nationalNumber: "asc" }),
-  });
+  const pokemons = await Pokemon.find().sort({ nationalNumber: "asc" });
+
+  try {
+    res.render("pokemon/index", {
+      title: "Pokémons",
+      baseUrl: req.baseUrl,
+      pokemons,
+    });
+  } catch (err) {
+    res.status(404).send({ message: "Pokemons not found !" });
+  }
 });
 
 router.get("/:idPokemon", async (req, res) => {
-  res.render("pokemon/show", {
-    title: 791 + " - " + "Solgaleo",
-    baseUrl: req.baseUrl,
+  const pokemon = await Pokemon.findOne({
+    nationalNumber: req.params.idPokemon,
   });
+
+  try {
+    res.render("pokemon/show", {
+      title: pokemon.nationalNumber + " - " + pokemon.name.fr,
+      baseUrl: req.baseUrl,
+      pokemon,
+      hundredExperienceYield: numeral(pokemon.hundredExperienceYield).format(
+        "0,0"
+      ),
+    });
+  } catch (err) {
+    res.status(404).send({ message: "Pokemon not found !" });
+  }
 });
 
 module.exports = router;
